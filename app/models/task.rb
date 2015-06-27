20332a852461c43f0c4d0ac36c9161b5c1faf73c
Michał Hartwich <michal.hartwich@gmail.com>
2015-06-27 13:23:41 +0200

    Change Status and Priority relationship

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,6 @@ class Task < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
   has_many :comments
-  has_one :status
-  has_one :priority
+  belongs_to :status
+  belongs_to :priority
 end
