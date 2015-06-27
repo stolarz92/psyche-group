918549a2e042a792f9370eaeca3c5bc8633a7ae2
Justyna <justyna.mianowska90@gmail.com>
2015-06-27 13:48:31 +0200

    Add comments controller and views

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,36 @@
+class CommentsController < ApplicationController
+  before_action :set_comment, only: [:update, :destroy]
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to @comment.resourceable, notice: 'Comment has been added.'
+    else
+      redirect_to @comment.resourceable, notice: 'Something went wrong.'
+    end
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to @comment.resourceable, notice: 'Comment has been updated.'
+    else
+      redirect_to @comment.resourceable, notice: 'Something went wrong.'
+    end
+  end
+
+  def destroy
+    resourceable = @comment.resourceable
+    @comment.destroy
+    redirect_to resourceable, notice: 'Comment has been removed.'
+  end
+
+  private
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content, :user_id, :task_id, :comment_id)
+  end
+end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,8 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :task
   belongs_to :project
+
+  def resourceable
+    task.present? ? task : project
+  end
 end
