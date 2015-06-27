138bd3419cd1cdc22e0aecce97daf47939fae77d
Radosław Stolarski <stolarz92@gmail.com>
2015-06-27 12:23:26 +0200

    Add devise, relationships, and gravtastic to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
+  include Gravtastic
+  gravtastic
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :tasks
+  has_many :projects, through: :membership
 end
