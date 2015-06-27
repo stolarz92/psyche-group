bada77f4de7d152d973131071ead018d945e743b
Radosław Stolarski <stolarz92@gmail.com>
2015-06-27 15:44:22 +0200

    Delete signed_in action

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -41,11 +41,6 @@ class ProjectsController < ApplicationController
 
   private
 
-  def signed_in
-    flash[:danger] = 'You have no permission.'
-    redirect_to new_user_session_path unless user_signed_in?
-  end
-
   def set_project
     @project = Project.find(params[:id])
   end
