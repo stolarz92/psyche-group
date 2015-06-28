d053d8c7b6fb33997f99155fad95160bdf399fd8
Radosław Stolarski <stolarz92@gmail.com>
2015-06-28 11:39:48 +0200

    Add project and taks to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,15 @@
 #   cities = City.where([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.where(name: 'Emanuel', city: cities.first)
 
-Priority.where(name: 'Low').first_or_create
+priority = Priority.where(name: 'Low').first_or_create
 Priority.where(name: 'Medium').first_or_create
 Priority.where(name: 'High').first_or_create
 
-Status.where(name: 'New').first_or_create
+status = Status.where(name: 'New').first_or_create
 Status.where(name: 'In progress').first_or_create
 Status.where(name: 'Closed').first_or_create
 
-User.find_or_create_by(email: 'user@example.com') do |u|
+user = User.find_or_create_by(email: 'user@example.com') do |u|
   u.password = 'dontknow'
   u.password_confirmation = 'dontknow'
 end
@@ -26,3 +26,19 @@ end
 Role.where(name: 'owner').first_or_create
 Role.where(name: 'contributor').first_or_create
 Role.where(name: 'admin').first_or_create
+
+project = Project.where(name: 'Test project',
+              description: 'Lorem ipsum',
+              starts_at: Time.now,
+              ends_at: Time.now + 3.day
+).first_or_create
+
+Task.where(
+         name: 'Test task',
+         description: 'Lorem ipsum',
+         deadline: Time.now + 2.day,
+         project: project,
+         user: user,
+         status: status,
+         priority: priority
+).first_or_create
